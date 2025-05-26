6ac115dfb69e892168dea84fb4812148b3d97bad
khafidhasukma <khafidhasukma@gmail.com>
2025-05-26 21:26:09 +0700

    feat: implement user authentication and product display features

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ProductModel;
 
 class Home extends BaseController
 {
+    protected $product;
+
+    function __construct()
+    {
+        $this->product = new ProductModel();
+    }
+    
     public function index(): string
     {
-        return view('v_home');
+
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_home', $data);
     }
 
     public function faq(): string
