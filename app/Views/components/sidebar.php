9a916985c547a64e9423db75f9b1f7af53558cb1
Khafidha Sukma <khafidhasukma@gmail.com>
2025-06-23 22:52:11 +0700

    feat: implement profile functionality with transaction history and details

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -40,7 +40,7 @@
         <i class="bi bi-person"></i>
         <span>Profile</span>
       </a>
-    </li> <!-- End Profile Nav -->
+    </li><!-- End Profile Nav -->
 
     <li class="nav-item">
       <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
